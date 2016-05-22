980f67707dbcc6cc21b9e283a94c0554604c6782
StefaniGit <StefaniGit@knallakoff.de>
2016-05-22 17:48:22 +0200

    optimise  LCD  to my special likeing

--- Marlin/ultralcd.cpp
+++ Marlin/ultralcd.cpp
@@ -1283,6 +1283,7 @@ static void _lcd_move_menu_axis() {
   if (_MOVE_XYZ_ALLOWED) {
     MENU_ITEM(submenu, MSG_MOVE_X, lcd_move_x);
     MENU_ITEM(submenu, MSG_MOVE_Y, lcd_move_y);
+	MENU_ITEM(submenu, MSG_MOVE_Z, lcd_move_z);  // Add z to 10 mm menu 
   }
   if (move_menu_scale < 10.0) {
     if (_MOVE_XYZ_ALLOWED) MENU_ITEM(submenu, MSG_MOVE_Z, lcd_move_z);
